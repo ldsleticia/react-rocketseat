e4a0af3dc2efbf8a31b3f85916b02fc1ca77b0d0
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-05-10 21:15:17 -0300

    adicionando repositorios

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,13 +1,11 @@
 export function RepositoryItem(props) {
   return (
     <li>
-      <strong>{props.repository?.name ?? "Default"}</strong>
-      <p>{props.repository?.description ?? "Default"}</p>
-      <a href={props.repository?.link ?? "Default"}>Acesse o repositório</a>
+      <strong>{props.repository?.name}</strong>
+      <p>{props.repository?.description}</p>
+      <a href={props.repository?.html_url}>
+        Acesse o repositório
+      </a>
     </li>
   );
 }
-
-// No trecho { props.repository?.name ?? 'Default'} o que estamos dizendo é
-// Se o repository.name for vazio, retornamos o valor 'Default'
-// Caso a propriedade .name não exista, ele também retorna 'Default'
